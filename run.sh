189201c0c78e8e735ab8b75d798ea7db3f6dd70b
AnCaPepe <leonardojc@protonmail.com>
2021-08-29 06:38:07 -0300

    Fix container name

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/devgrid-challenge
 docker-compose build
 docker-compose up -d
-docker exec -it flask-rest-server-weather_client_1 echo "$( getent hosts flaskapp | head -n 1 | cut -d ' ' -f 1 ) server" >> /etc/hosts
+docker exec -it devgrid-challenge_client_1 echo "$( getent hosts flaskapp | head -n 1 | cut -d ' ' -f 1 ) server" >> /etc/hosts
