f33d58af48f9dee6c94649f213fb735fa6e84dec
AnCaPepe <leonardojc@protonmail.com>
2021-08-29 06:49:15 -0300

    Fix container command

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/devgrid-challenge
 docker-compose build
 docker-compose up -d
-docker exec -it devgrid-challenge_client_1 echo "$( getent hosts flaskapp | head -n 1 | cut -d ' ' -f 1 ) server" >> /etc/hosts
+docker exec devgrid-challenge_client_1 sh -c 'echo "$( getent hosts flaskapp | head -n 1 | cut -d '"' '"' -f 1 ) server" >> /etc/hosts'
