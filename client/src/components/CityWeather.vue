11f809a90edfd33e79ca88ff20aa8e548e268229
AnCaPepe <leonardojc@protonmail.com>
2021-08-29 00:36:16 -0300

    Add city's weather display component

---
+++ client/src/components/CityWeather.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>{{ city }}</h3>
+    <h1>{{ temperature-273 }}°C</h1>
+    <h3>{{ weather }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityWeather",
+  props: {
+    city: String,
+    temperature: Number,
+    weather: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  border-bottom: 1px solid black;
+  background: lightgrey;
+  margin: 5px;
+  width: 150px;
+  text-align: center;
+  border-radius: 5px;
+  display: inline-block;
+}
+h3 {
+  margin: 10px;
+}
+</style>
