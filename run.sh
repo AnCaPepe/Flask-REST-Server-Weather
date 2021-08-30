dd049d1462578cc23dcab5957ef3f4a118009ea6
AnCaPepe <leonardojc@protonmail.com>
2021-08-29 22:19:39 -0300

    Reverting changes. Confused container host resolution with browser host resolution

--- run.sh
+++ run.sh
@@ -2,4 +2,3 @@
 cd /home/ec2-user/devgrid-challenge
 docker-compose build
 docker-compose up -d
-docker exec devgrid-challenge_client_1 sh -c 'echo "$( getent hosts flaskapp | head -n 1 | cut -d '"' '"' -f 1 ) server" >> /etc/hosts'
